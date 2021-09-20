b7c249bbd55f6a3bd8cebfaf5990233f9539b21b
fadhilansyah25 <fadhilansyah25@gmail.com>
2021-09-20 09:13:24 +0700

    Config connection to database

---
+++ Configs/Database.go
@@ -0,0 +1,55 @@
+package configs
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// DBConfig represents db configuration
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	DBName   string
+	Password string
+}
+
+func BuildDBConfig() *DBConfig {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	host := os.Getenv("HOST")
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbName := os.Getenv("DBNAME")
+
+	dbConfig := DBConfig{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DBName:   dbName,
+	}
+	return &dbConfig
+}
+func DbURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
+	)
+}
