1ff0fb927f493a1d5b1b418d6ea97a106c0603f4
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2021-10-01 16:29:13 +0700

    Update Docker file and Docker-compose

--- Routes/Login/Route.go
+++ Routes/Login/Route.go
@@ -12,8 +12,8 @@ func UserLoginRoute(route *echo.Group) {
 	jwtSecretKey := os.Getenv("SECRET_JWT")
 	jwt := middleware.JWT([]byte(jwtSecretKey))
 
-	route.POST("create-login", Controllers.CreateUserLogin, jwt)
-	route.GET("user-login", Controllers.GetAllUserLogin, jwt)
+	route.POST("create-login", Controllers.CreateUserLogin)
 	route.POST("login", Controllers.UserLogin)
+	route.GET("user-login", Controllers.GetAllUserLogin, jwt)
 	route.PUT("user-login/:id", Controllers.UpdateUserLogin, jwt)
 }
